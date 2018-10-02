1384dcf13a550641d4069bc1a9865a18ec49c697
Attila Csaba Marosi <a.cs.marosi@gmail.com>
2018-10-02 09:05:24 +0200

    fix: error and helper messages

--- typepaste.py
+++ typepaste.py
@@ -10,16 +10,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='TypePaste - type clipboard '
                                                  'data into active window')
     parser.add_argument('-B', '--batch-size', metavar='batch_size',
-                        help="Print number of characters at once (batch)")
+                        help="print number of characters at once (batch)")
     args = parser.parse_args()
     batch_size = 0
     if not args.batch_size:
-        sys.exit("ERROR: Batch size (-B or --batch-size) should be at "
-                 "least 1.")
+        sys.exit("ERROR: Batch size (-B or --batch-size) missing.")
     try:
         batch_size = int(args.batch_size)
     except ValueError:
-        sys.exit("Batch size (-B or --batch) should be at least 1.")
+        sys.exit("Batch size (-B or --batch-size) should be an integer "
+                 "at least 1.")
     clipboard_text = clipboard.paste()
     text = ""
     for i in clipboard_text:
